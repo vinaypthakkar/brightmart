7ba1e53196e9eedd3f57861d117f4c0fc34c56b4
vinaypthakkar <265751003+vinaypthakkar@users.noreply.github.com>
2026-09-17 16:51:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 # Load the saved model
-model = joblib.load(open('linear_reg.sav', 'rb'))
+model = joblib.load(open('linear.sav', 'rb'))
 
 st.title('Sales Prediction App')
 
